0e9b2fc9458255fbe8fe70d61489c913cbc2002f
Muhammad Rizky Prayoga_024 <mrpharyga2019@gmail.com>
2022-10-13 10:53:12 +0700

    TUGAS1_DAA1_201401024
    TUGAS 1 LAB DAA1

---
+++ Tugas1/TUGAS1_DAA1_201401024.cpp
@@ -0,0 +1,88 @@
+//NAMA : MUHAMMAD RIZKY PRAYOGA
+//NIM  : 201401024
+//LAB  : DAA1
+
+#include <iostream>
+using namespace std;
+
+struct BST
+{
+	int data;
+	BST* kiri;
+	BST* kanan;
+};
+
+BST* InsertNode(int data, BST* tree)
+{
+	if(tree==NULL)
+	{
+		BST* NewNode = new BST;
+		NewNode->data = data;
+		NewNode->kiri = NULL;
+		NewNode->kanan = NULL;
+		tree = NewNode;
+		cout<<NewNode->data;//Menampilkan binary tree
+	}
+	else if(tree->data > data)
+	{
+		if(tree->kiri == NULL || tree->kiri->data < data||tree->kiri->data > data)
+		{
+			cout<<endl;//Memberi jarak antara binary atas dengan binary di bawahnya
+			tree->kiri = InsertNode(data, tree->kiri);
+		}		
+	}
+	else
+	{
+		if(tree->kanan == NULL||  tree->kanan->data > data)
+		{
+			cout<<" ";// Memberi jarak antara binary sebelah kiri dengan binary sebelah kanan
+			tree->kanan = InsertNode(data, tree->kanan);
+		}
+	}
+	return tree;
+}
+
+bool SearchNode(int data, BST* tree)
+{
+	if(tree==NULL) return false;
+	else if(tree->data == data) return true;
+	else if(tree->data < data) return SearchNode(data, tree->kanan);
+	else return SearchNode(data, tree->kiri);
+}
+
+void PrintLeftNodes(BST* tree)
+{
+	if(tree!=NULL)
+	{
+		cout<<tree->data;
+		PrintLeftNodes(tree->kiri);
+	}
+	
+}
+
+int main()
+{
+	int a[10] = {4,1,9,6,2,5,3,8,7,0};
+	BST* Pohon = NULL;
+	
+	cout<<"BINARY TREE : "<<endl;
+	cout<<"--------------"<<endl;
+	
+	for(int i=0;i<10;i++)
+	{
+		Pohon = InsertNode(a[i], Pohon);
+	}
+	
+	cout<<endl<<endl;
+	cout<<"BINARY SEARCH TREE"<<endl;
+	cout<<"------------------"<<endl;
+	cout<<"Search : 7 "<<endl;
+	cout<<"Hasil : ";
+	if(!SearchNode(7, Pohon)) cout<<"Tidak ";
+	cout<<"Ada 7"<<endl;
+	cout<<endl;
+	cout<<"Search : 11 "<<endl;
+	cout<<"Hasil : ";
+	if(!SearchNode(11, Pohon)) cout<<"Tidak ";
+	cout<<"Ada 11"<<endl;
+}   
